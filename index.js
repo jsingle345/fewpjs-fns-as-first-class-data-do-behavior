f162344c58120dfac9df5ea55a11d5d5ea1d4a00
jsingle345 <jsingle345@gmail.com>
2020-11-01 11:42:51 -0600

    commit tree 75984e389a991ad4294cb6effc0edc9a817f5603

--- index.js
+++ index.js
@@ -10,4 +10,48 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+    greet = (time) => {
+      const mytime = parseInt(time.split(":")[0])
+      
+      if (mytime < 12){
+      return `Good Morning`
+      }
+      else if (mytime > 17){
+      return `Good Evening`
+      }
+      else 
+      return `Good Afternoon`
+
+    }
+
+
 /* Write your implementation of displayMessage() */
+    function displayMessage(msg){
+       greeting = document.querySelector('#greeting');
+      greeting.innerText = (msg);
+    }
+
+
+  //   * Write your implementation of displayMessage() */
+  //  //   function displayMessage(msg){
+  //  //       greeting = document.getElementById('greeting');
+  //  //          greeting.innerText = msg
+            
+  //  //      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
